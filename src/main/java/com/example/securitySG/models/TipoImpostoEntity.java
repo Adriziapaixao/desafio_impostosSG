66a791af4efed1319369cd6fab901d8597d09a1f
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-17 13:53:09 -0300

    feat: adiciona TipoImpostoEntity com anotações JPA

---
+++ src/main/java/com/example/securitySG/models/TipoImpostoEntity.java
@@ -0,0 +1,26 @@
+package com.example.securitySG.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@Data
+@Table(name = "tipos_impostos")
+@Entity
+public class TipoImpostoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Double rate;
+}
