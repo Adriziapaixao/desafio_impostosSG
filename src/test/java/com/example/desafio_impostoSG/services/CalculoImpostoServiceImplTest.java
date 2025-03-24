e39669c2cc1f8171344a4be00f3df1e3e8eaceaf
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-24 14:17:56 -0300

    test(CalculoImpostoServiceImplTest): adiciona testes para validar cenários de cálculo de imposto
    - Adicionado teste para verificar exceção ao não encontrar tipo de imposto.
    - Garantido que o método findById é chamado e validado corretamente.
    - Adicionado verificação de interações com o repositório para maior cobertura.

--- src/test/java/com/example/desafio_impostoSG/services/CalculoImpostoServiceImplTest.java
+++ src/test/java/com/example/desafio_impostoSG/services/CalculoImpostoServiceImplTest.java
@@ -1,14 +1,90 @@
 package com.example.desafio_impostoSG.services;
 
+
+import com.example.desafio_impostoSG.dtos.CalculoImpostoResponseDto;
+import com.example.desafio_impostoSG.models.TipoImpostoEntity;
+import com.example.desafio_impostoSG.repostories.CalculoImpostoRepository;
+
+import com.example.desafio_impostoSG.repostories.TipoImpostoRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class CalculoImpostoServiceImplTest {
 
+    @Mock
+    private CalculoImpostoRepository calculoRepository;
+
+    @Mock
+    private TipoImpostoRepository tipoImpostoRepository;
+
+    @InjectMocks
+    CalculoImpostoServiceImpl calculoImpostoService;
+
     @Test
-    void given_calcularImpostoDetalhado_whenValorBaseMenorIgualaZero_thenLancaEntityNotFoundException()  {
+    void given_valorBase_whenValorBaseMenorIgualaZero_thenLancaIllegalArgumentException()  {
+
+        Long tipoImpostoId = 1L;
+        double valorBase = 0.0;
 
 
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculoImpostoService.calcularImpostoDetalhado( 1L, 0.0);
+        });
+
+        Assertions.assertEquals("O valor base deve ser maior que zero.", exception.getMessage());
+
+        verifyNoMoreInteractions(calculoRepository);
+    }
+
+    @Test
+    void given_tipoImpostoId_whenTipoImpostoNaoEncontrado_thenLancaRuntimeException() {
+
+        Long tipoImpostoId = 1L;
+        double valorBase = 150.0;
+
+        when(tipoImpostoRepository.findById(1L)).thenReturn(Optional.empty());
+
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> {
+            calculoImpostoService.calcularImpostoDetalhado(tipoImpostoId, valorBase);
+        });
+
+        Assertions.assertEquals("Tipo de imposto não encontrado", exception.getMessage());
+
+        verify(tipoImpostoRepository).findById(tipoImpostoId);
+        verifyNoMoreInteractions(tipoImpostoRepository);
+
+    }
+
+    @Test
+    void given_valorBaseAndTipoImpostoId_whenValid_thenCalculaImpostoCorretamente() {
+        Long tipoImpostoId = 1L;
+        double valorBase = 100.0;
+
+        TipoImpostoEntity impostoEntity = new TipoImpostoEntity();
+        impostoEntity.setId(1L);
+        impostoEntity.setName("ICMS");
+        impostoEntity.setRate(18.0);
+
+        when(tipoImpostoRepository.findById(1L)).thenReturn(Optional.of(impostoEntity));
+        CalculoImpostoResponseDto calculoImpostoResponse = calculoImpostoService.calcularImpostoDetalhado(tipoImpostoId, valorBase);
+
+        Assertions.assertNotNull(calculoImpostoResponse);
+        Assertions.assertEquals("ICMS", calculoImpostoResponse.getTipoImposto());
+        Assertions.assertEquals(100.0, calculoImpostoResponse.getValorBase());
+        Assertions.assertEquals(18.0, calculoImpostoResponse.getRate());
+        Assertions.assertEquals(18.0, calculoImpostoResponse.getValorImposto());
+
+        verify(tipoImpostoRepository).findById(tipoImpostoId);
+        verifyNoMoreInteractions(tipoImpostoRepository);
     }
 }
