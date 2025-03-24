17474e3095baefa623b2855d70e47aa53e1f60c6
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-03-24 11:16:43 -0300

    build(jacoco): adiciona plugin Jacoco para análise de cobertura de testes
    - Configura o plugin Jacoco no pom.xml para geração de relatórios de cobertura.
    - Define as configurações padrão para execução durante a fase de teste.
    - Garante integração com ferramentas de CI para validação de cobertura mínima.
    - Documenta a configuração no arquivo pom.xml para facilitar manutenção futura.

---
+++ src/test/java/com/example/desafio_impostoSG/services/CalculoImpostoServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.example.desafio_impostoSG.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculoImpostoServiceImplTest {
+
+    @Test
+    void given_calcularImpostoDetalhado_whenValorBaseMenorIgualaZero_thenLancaEntityNotFoundException()  {
+
+
+    }
+}
